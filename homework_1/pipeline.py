dc7136f0d6c3049bbae7decc3ec935179e401acc
Casey Cooper <caseykcooper14@gmail.com>
2023-09-08 16:14:21 -0600

    Update pipeline to include modules, Update Docker entrypoint

--- homework_1/pipeline.py
+++ homework_1/pipeline.py
@@ -3,35 +3,50 @@
 # winpty docker run -it container_name arg1 arg2
 
 import sys
-
 import pandas as pd
 
-print(sys.argv) # prints the name of the file and the command line argument(s)
-
-filename = sys.argv[1] # gets the first command line argument (name of the dataset file)
+# ---------------------------------------------------------------------------------------- #
+# READ IN DATA
+def load_data(filename):
+    print('Loading Data...')
+    df = pd.read_csv(filename) # read in csv based on command line argument
+    return df
 
 # ---------------------------------------------------------------------------------------- #
 # DATASET CHANGES
+def transform_data(dataframe):
+    print('Transforming Data...')
+    altered_df = dataframe.copy()
+    # select only cats that are of the domestic shorthair breed
+    altered_df = altered_df.loc[altered_df['Animal Type'] == 'Cat']
+    altered_df = altered_df.loc[(altered_df['Breed'] == 'Domestic Shorthair Mix') | (altered_df['Breed'] == 'Domestic Shorthair')]
 
-df = pd.read_csv(filename) # read in csv based on command line argument
+    # drop rows with missing values
+    altered_df.dropna(axis=0, inplace = True)
+    # reset the index of the dataframe
+    altered_df.reset_index(inplace=True, drop=True)
 
-altered_df = df.copy()
+    return altered_df
 
-# select only cats that are of the domestic shorthair breed
-altered_df = altered_df.loc[df['Animal Type'] == 'Cat']
-altered_df = altered_df.loc[(df['Breed'] == 'Domestic Shorthair Mix') | (df['Breed'] == 'Domestic Shorthair')]
-
-# drop rows with missing values
-altered_df.dropna(axis=0, inplace = True)
-# reset the index of the dataframe
-altered_df.reset_index(inplace=True, drop=True)
 
 # ---------------------------------------------------------------------------------------- #
 # SAVE ALTERED DATA TO .CSV
+def save_data(dataframe, filename):
+    print('Saving Data...')
+    dataframe.to_csv(filename)
 
-# gets the second command line argument (name of the altered dataset file to create)
-altered_filename = sys.argv[2]
 
-altered_df.to_csv(altered_filename)
+if __name__ == "__main__":
+    print('Starting...')
 
-print(f'job finished successfully')
+    print(sys.argv) # prints the name of the file and the command line argument(s)
+    # gets the first command line argument (name of the dataset file)
+    filename = sys.argv[1]
+    # gets the second command line argument (name of the altered dataset file to create)
+    save_filename = sys.argv[2]
+
+    df = load_data(filename)
+    altered_df = transform_data(df)
+    save_data(altered_df, save_filename)
+
+    print(f'Job finished successfully!')
